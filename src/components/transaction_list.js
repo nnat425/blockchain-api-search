c269dd06588a2f6b0f756ce6dab820fa6d97d29a
Navraj Nat <nnat-consultant@scholastic.com>
2018-02-15 17:44:33 -0500

    create transaction list fucntional component

---
+++ src/components/transaction_list.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TransactionList = () =>{
+	return(
+	  <ul className="col-md-4 list-group">
+
+	  </ul>
+	)
+}
+
+export default TransactionList;
--- src/index.js
+++ src/index.js
@@ -2,20 +2,29 @@ import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import Blockchain from 'blockchain.info/blockexplorer';
 import SearchBar from './components/search_bar';
+import TransactionList from './components/transaction_list';
 
 const API_URL = "https://blockchain.info" 
 
 
- Blockchain.getAddress("1BoatSLRHtKNngkdXEeobR76b53LETtpyT").then(function(result){
- 	console.log(result);
- });
+
   
 
 class App extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = { transactions: [] };
+
+		Blockchain.getAddress("1BoatSLRHtKNngkdXEeobR76b53LETtpyT").then(function(data){
+ 			this.setState({transactions:data.txs})
+ 		});
+	}
   render(){
 	return (
 		<div> 
 		  <SearchBar />
+		  <VideoList videos={this.state.transactions} />
 		</div>
 	);  //This is jsx, babel does compiling to convert it to javascript
   }
