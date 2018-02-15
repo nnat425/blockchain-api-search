43fbd72c6e0f67b843fd547d7e714c7c7bcff6ab
Navraj Nat <nnat-consultant@scholastic.com>
2018-02-14 20:36:30 -0500

    add searchBar component inside of app

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ import SearchBar from './components/search_bar';
 const API_URL = "https://blockchain.info" 
 
 const App = () => {
-	return <div> Hi! </div>;  //This is jsx, babel does compiling to convert it to javascript
+	return (
+		<div> 
+		<SearchBar />
+		</div>
+	 		);  //This is jsx, babel does compiling to convert it to javascript
 }
 
 //Need to instantiate components before rendering to DOM
