3a932b8076cec7f4b83b2ddfc402dc6ce81e342e
Navraj Nat <nnat-consultant@scholastic.com>
2018-02-15 17:27:20 -0500

    change functional component to class component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import Blockchain from 'blockchain.info/blockexplorer';
-
 import SearchBar from './components/search_bar';
 
 const API_URL = "https://blockchain.info" 
@@ -10,14 +9,16 @@ const API_URL = "https://blockchain.info"
  Blockchain.getAddress("1BoatSLRHtKNngkdXEeobR76b53LETtpyT").then(function(result){
  	console.log(result);
  });
- 
+  
 
-const App = () => {
+class App extends Component {
+  render(){
 	return (
 		<div> 
-		<SearchBar />
+		  <SearchBar />
 		</div>
 	);  //This is jsx, babel does compiling to convert it to javascript
+  }
 }
 
 //Need to instantiate components before rendering to DOM
