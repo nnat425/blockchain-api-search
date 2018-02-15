e5bb399dc3f3978266d82b7e941999ca0c171a58
Navraj Nat <nnat-consultant@scholastic.com>
2018-02-15 18:15:20 -0500

    display Transaction 50 times on browser

--- src/components/transaction_list.js
+++ src/components/transaction_list.js
@@ -3,9 +3,13 @@ import TransactionListItem from './transaction_list_item';
 
 const TransactionList = (props) => {
 
+	const transactionItems = props.transactions.map((transaction) => {
+		return <TransactionListItem transaction={transaction} />
+	});
+
 	return(
 	  <ul className="col-md-4 list-group">
-	  	{props.transactions.length}
+	  	{transactionItems}
 	  </ul>
 	)
 }
---
+++ src/components/transaction_list_item.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TransactionListItem = (props) => {
+	return <li>Transaction</li>
+};
+
+export default TransactionListItem;
